4cb8031cab5c9b9fd3e77da6082929e34f64d43c
Yasinehsan <yasinehsan11@gmail.com>
2019-03-23 12:55:53 -0400

    added auto-layout

--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -23,10 +23,11 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
         
-        layout.minimumLineSpacing = 40
-        layout.minimumInteritemSpacing = 40
+        layout.minimumLineSpacing = 4
+        layout.minimumInteritemSpacing = 4
         
-        let width = view.frame.size.width / 3
+        let width = (view.frame.size.width - layout.minimumInteritemSpacing * 2) / 3
+        
         layout.itemSize = CGSize(width: width, height: width * 3 / 2)
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
