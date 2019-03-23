f15035a195166c70aef705e6e1c3222c3f434a99
Yasinehsan <yasinehsan11@gmail.com>
2019-03-23 12:34:20 -0400

    added collection VC

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by A. Rodgers on 3/23/19.
+//  Copyright © 2019 Yasin Ehsan. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
