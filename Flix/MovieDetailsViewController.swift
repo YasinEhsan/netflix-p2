64c08896540d65c39a216301eae3153c74a0bea8
Yasinehsan <yasinehsan11@gmail.com>
2019-03-23 11:31:09 -0400

    added backdrop

--- Flix/MovieDetailsViewController.swift
+++ Flix/MovieDetailsViewController.swift
@@ -6,18 +6,17 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MovieDetailsViewController: UIViewController {
     
     
     @IBOutlet weak var backdropView: UIImageView!
-    
     @IBOutlet weak var posterView: UIImageView!
-    
     @IBOutlet weak var titleLabel: UILabel!
-    var movie: [String:Any]!
-
     @IBOutlet weak var synopsisLabel: UILabel!
+    
+    var movie: [String:Any]!
     
     
     override func viewDidLoad() {
@@ -25,7 +24,19 @@ class MovieDetailsViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         
-        print(movie["title"])
+        
+        titleLabel.text = movie["title"] as? String
+        synopsisLabel.text = movie["overview"] as? String
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        posterView.af_setImage(withURL: posterUrl!)
+       
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: baseUrl + backdropPath)
+        backdropView.af_setImage(withURL: backdropUrl!)
     }
     
 
